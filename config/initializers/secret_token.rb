e535bf66df0c2d05f4ac2715934091ea35f3b965
John Murphy <murphybytes@gmail.com>
2012-12-27 20:30:58 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linknsynch::Application.config.secret_token = 'b759e4c38cde18d56bdfebd8b967db511274df4624e3b0672f0a0a6c8d50331ed1321daa164e20b303c36c86a879cb98c967d250cca20a131220cd538aaeb580'
