06d11e68ac5dd4edda476bec2fb034512cd256fb
John Murphy <murphybytes@gmail.com>
2013-01-19 15:46:26 -0600

    added calculator

---
+++ lib/pqr/calculator.rb
@@ -0,0 +1,16 @@
+module PQR
+
+  class Calculator
+    attr_reader :total_kw_generated
+
+    def initialize( opts = {}  )
+      @total_kw_generated = 0
+    end
+
+    def run
+    end
+
+    
+  end
+
+end
--- test/unit/lib/calculator_test.rb
+++ test/unit/lib/calculator_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
+$: << Dir.pwd
+require 'lib/pqr/calculator'
 
 class CalculatorTest < ActiveSupport::TestCase
   test "calculator" do
-    assert true
+    calculator = PQR::Calculator.new( {} )
+    calculator.run
+    assert calculator.total_kw_generated > 0
   end
 
 end
