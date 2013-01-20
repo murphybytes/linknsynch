9bf1f93ee1cefa32da037e38c277a1c551cd0b58
John Murphy <murphybytes@gmail.com>
2013-01-19 19:40:29 -0600

    got test fetching samples to work

--- test/unit/lib/calculator_test.rb
+++ test/unit/lib/calculator_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
-$: << Dir.pwd
-require 'lib/pqr/calculator'
+
 
 class CalculatorTest < ActiveSupport::TestCase
+  def setup
+    @@set ||= SetMeta.where( :name => 'TestSample' ).first
+  end
+
+  test "Verify Sample Count" do
+    assert_equal 8760, @@set.samples.size, "sample count"
+  end
+
   test "calculator" do
     calculator = PQR::Calculator.new( {} )
     calculator.run
