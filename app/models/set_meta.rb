13a40913f8b159089f1d0c03066f1445ec270287
John Murphy <murphybytes@gmail.com>
2013-05-31 16:40:35 -0500

    wired up lmp calculations

--- app/models/set_meta.rb
+++ app/models/set_meta.rb
@@ -3,4 +3,14 @@ class SetMeta < ActiveRecord::Base
   has_many :samples, :autosave => true, :dependent => :delete_all
   belongs_to :user
   validates :name, :uniqueness => true
+  
+  def get_first_sample_date
+    Sample.minimum( 'sample_time', :conditions => { :set_meta_id => self.id } )
+  end
+  
+  def get_ceiling_sample_date
+    dt = Sample.maximum( 'sample_time', :conditions => { :set_meta_id => self.id } ) 
+    dt += 1.hour
+  end
+
 end
--- test/unit/set_meta_test.rb
+++ test/unit/set_meta_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SetMetaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get first sample date" do
+    sm = SetMeta.find( 1 )
+    expected = DateTime.parse( "2011-01-01 00:00:00" )
+    assert_equal expected , sm.get_first_sample_date
+  end
+
+  test "get ceiling sample date" do
+    sm = SetMeta.find( 1 )
+    expected = DateTime.parse( "2012-01-01 00:00:00" )
+    assert_equal expected, sm.get_ceiling_sample_date 
+  end
+
+
+
+  
 end
