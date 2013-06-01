a06da26a53dc1078e7d1a099729e61c3558dc315
John Murphy <murphybytes@gmail.com>
2013-06-01 09:31:38 -0500

    added scoped uniqueness contraint on LMP

--- app/models/location_marginal_price.rb
+++ app/models/location_marginal_price.rb
@@ -1,6 +1,6 @@
 class LocationMarginalPrice < ActiveRecord::Base
   attr_accessible :node_id, :period, :value
-
+  validates :period, :uniqueness => { :scope => :node_id }
   belongs_to :node
 
 end
